be5cd72513a612f3222ed09fbd7b6798ee803436
Kelley Au <kau@idgcommunications.com>
2020-04-19 19:05:41 -0700

    renamed index.js to main.js, added frameless and moveable dragable window

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+//This just defines maiNWindow so that we can scope it globally
+//let mainWindow;
+
+app.on('ready', () => {
+    console.log('App is ready');
+
+    app.dock.hide()
+    let alwaysOnTopWindow = new BrowserWindow({ width: 300, height: 150, frame: false, movable: true, resizable: false })
+    alwaysOnTopWindow.setAlwaysOnTop(true, 'floating')
+    alwaysOnTopWindow.setVisibleOnAllWorkspaces(true)
+    alwaysOnTopWindow.setFullScreenable(false)
+    alwaysOnTopWindow.show()
+    alwaysOnTopWindow.setVisibleOnAllWorkspaces(false)
+    alwaysOnTopWindow.loadURL(`file://${__dirname}/index.html`);
+    app.dock.show()
+
+});
